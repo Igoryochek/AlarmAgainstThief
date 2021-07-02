b72562f01b8d65d36b502ba72f279f4980c69a51
Igoryochek <83440261+Igoryochek@users.noreply.github.com>
2021-07-02 22:22:46 +1000

    Change

--- Assets/Scripts/VolumeChanging.cs
+++ Assets/Scripts/VolumeChanging.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 
 public class VolumeChanging : MonoBehaviour
 {
+    [SerializeField] private float _speedOfChanging;
     private AudioSource _source;
-    private float _volume;
-    private float _startVolume;
     private float _endVolume;
     private Coroutine _changeVolume;
 
@@ -19,33 +18,28 @@ public class VolumeChanging : MonoBehaviour
 
     public void PlaySound()
     {
-        if (_changeVolume!=null)
+        if (_changeVolume != null)
         {
             StopCoroutine(_changeVolume);
         }
 
-        _startVolume = _source.volume;
         _endVolume = 1f;
-        _changeVolume=StartCoroutine(ChangeVolume(_endVolume));
+        _changeVolume = StartCoroutine(ChangeVolume(_endVolume));
     }
 
     public void StopSound()
     {
-        StopCoroutine(_changeVolume);
-        _startVolume = _source.volume;
-        _endVolume =0f;
-        _changeVolume= StartCoroutine( ChangeVolume(_endVolume));
+        StopCoroutine(ChangeVolume(_endVolume));
+        _endVolume = 0f;
+        _changeVolume = StartCoroutine(ChangeVolume(_endVolume));
     }
 
     private IEnumerator ChangeVolume(float endVolume)
     {
-        _volume = 0;
-        
-        while (endVolume!=_source.volume)
+        while (endVolume != _source.volume)
         {
-                _volume +=Time.deltaTime;
-                _source.volume = Mathf.Lerp(_startVolume, _endVolume, _volume);
-                yield return null;
-        } 
+            _source.volume = Mathf.MoveTowards(_source.volume, _endVolume, _speedOfChanging * Time.deltaTime);
+            yield return null;
+        }
     }
 }
